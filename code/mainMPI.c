169ec3624900f57856dbe9040a513a32a305f0fb
Fredrik <fny005@post.uit.no>
2022-09-30 11:33:57 +0200

    .

--- code/mainMPI.c
+++ code/mainMPI.c
@@ -5,56 +5,9 @@
 #include "crackme.h"
 #include "time.h"
 
-void recursive_crack(int rank, int depth, char *guess, int size, int num_threads, int hit) {
-
-    int new_depth = depth + 1;
-
-	if (new_depth == 1) {
-
-		int div = 256 / num_threads;
-		int start = rank * div;
-        int limit = start + div; 
-
-		printf("div: %d\nstart: %d\nlimit: %d\n", div, start, limit);
-		int i = 0;
-		while (i < limit) {
-			// printf("i: %d\n", i);
-			guess[depth] = i;
-		
-			recursive_crack(rank, new_depth, guess, size, num_threads, hit);
-			if (hit == 1)
-				return;
-
-			i++;
-		}
-
-    } else {
-
-		if (depth >= size) {
-			// printf("depth >= size");
-
-			if (p(size, guess) == 0) {
-				printf("HIT\n");
-				hit = 1;
-				return;
-			} 
-			return;
-		}
-	
-		for (int i = 0; i < 256; i++) {
-			// printf("i: %d\n", i);
-		
-			guess[depth] = i;
-			
-			recursive_crack(rank, new_depth, guess, size, num_threads, hit);
-			if (hit == 1)
-				return;
-		}
-	}
-}
-
 void iterative_crack(unsigned char *guess, int sizepass, int rank, int num_threads, unsigned long long starttime) {
 
+	/* Each process calculates what part of the possible characters to check. */
 	int num_chars = 128;
 	long size = pow(num_chars, sizepass);
 	long div = num_chars / num_threads;
@@ -64,36 +17,72 @@ void iterative_crack(unsigned char *guess, int sizepass, int rank, int num_threa
 	// printf("start: %d\nlimit: %d\n", start, limit);
 	long i = start;
 	
+
 	while (i < size) {
 		
+
+		/* Checks if current guess is the correct guess. */
 		if (p(sizepass, guess) == 0) {
 
 			unsigned long long end = gettime();
-
+			
 			int total = 0;
 			for (int i = 0; i < sizepass; i++) {
 			total += guess[i];
 			}
-			// printf("guess: %d\n", total);
+			printf("guess: %d\n", total);
 
 			printf("HIT!\n");
 			printf("time: %llu\n", (end - starttime));
 
+			/* If guess is correct, all processes are aborted. */
 			MPI_Abort(MPI_COMM_WORLD, 0);
 			return;
 		}
 
-		for (int index = sizepass - 1; index >= 0; index--) {
+		/* Iterates through each index in the char array, starting with the upmost index. */
+		// for (int index = sizepass - 1; index >= 0; index--) {
+
+		// 	// if (rank == 0) 
+		// 	// 	printf("index: %d, guess[index]: %d | num_chars - 1: %d\n", index, (int)guess[index], num_chars - 1);
+
+		// 	// 	for (int i = 0; i < sizepass; i++) {
+		// 	// 		printf("%d\n", guess[i]);
+		// 	// 	}
+		// 	// 	printf("\n");
+
+		// 	if (index == sizepass - 1) {
+
+		// 		if (guess[index] < limit - 1) {
+		// 			guess[index] += 1;
+		// 			break;
+		// 		} else {
+		// 			guess[index] = start;
+		// 		}
+
+		// 	} else {
+
+		// 		if (guess[index] < num_chars - 1) {
+		// 			guess[index] += 1;
+		// 			break;
+		// 		} else {
+		// 			guess[index] = 0;
+		// 		}
+
+		// 	}
+		// }
+
+		for (int index = 0; index >= sizepass - 1; index++) {
 
 			// if (rank == 0) 
 			// 	printf("index: %d, guess[index]: %d | num_chars - 1: %d\n", index, (int)guess[index], num_chars - 1);
 
-			// 	for (int i = 0; i < num_chars; i++) {
-			// 		printf("%d", guess[i]);
+			// 	for (int i = 0; i < sizepass; i++) {
+			// 		printf("%d\n", guess[i]);
 			// 	}
 			// 	printf("\n");
 
-			if (index == sizepass - 1) {
+			if (index == 0) {
 
 				if (guess[index] < limit - 1) {
 					guess[index] += 1;
